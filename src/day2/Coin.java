6082bc8772df420fbe627ae33e2b629b6e878421
Administrator <Administrator@user>
2016-07-31 15:28:02 +0900

    return, cahce 추가

--- src/day2/Coin.java
+++ src/day2/Coin.java
@@ -3,13 +3,16 @@ package day2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Coin {
 	private static int N = 0;
 	private static int K = 0;
-	private static int COUNT = 0;
 	private static int[] COINS = null;
+	private static int GAP = 1000;
+	private static Map<Integer, Integer> mem = new HashMap<>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -23,27 +26,34 @@ public class Coin {
 			COINS[i] = Integer.parseInt(br.readLine());
 		}
 		
-		getCase(0, K);
-		System.out.println(COUNT);
+		System.out.println(getCase(0, K));
 		br.close();
 	}
 	
-	private static void getCase(int idx, int rest){
-		
-		if(rest==0){
-			COUNT++;
-			return;
+	private static int getCase(int idx, int rest){
+		int result = 0;
+		int key = rest*GAP+idx;
+		if(mem.containsKey(key)){
+			return mem.get(key);
 		}
-		
-		if(idx == N)
-			return;
-		
-		if(rest < COINS[idx])
-			getCase(idx+1, rest);
-		
-		int maxCount = rest/COINS[idx];
-		for(int i=0; i<=maxCount; ++i){
-			getCase(idx+1, rest-(i*COINS[idx]));
+		else if(rest==0){
+			result = 1;
 		}
+		else if(idx == N){
+			result = -1;
+		}
+		else if(rest < COINS[idx]){
+			result = getCase(idx+1, rest);
+		}
+		else{
+			int maxCount = rest/COINS[idx];
+			for(int i=0; i<=maxCount; ++i){
+				int temp = getCase(idx+1, rest-(i*COINS[idx]));
+				if(temp>0)
+					result+=temp;
+			}
+		}
+		mem.put(rest*GAP+idx, result);
+		return result;
 	}
 }
