2644581be39c064300924173e01e5b21e7d3b68b
진유리 <yuriya.jin@samsung.com>
2016-08-17 11:41:23 +0900

    woods

---
+++ src/day4/Wood.java
@@ -0,0 +1,57 @@
+package day4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Wood {
+	private static int[] WOODS = null;
+	private static int M = 0;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		WOODS = new int[N];
+		int maxH = Integer.MIN_VALUE;
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; ++i){
+			WOODS[i] = Integer.parseInt(st.nextToken());
+			maxH = Math.max(maxH, WOODS[i]);
+		}
+		
+		System.out.println(binarySearch(1, maxH));
+		br.close();
+	}
+	
+	private static int binarySearch(int buttom, int top){
+		int mid = (top+buttom)/2;
+		long total = getH(mid);
+		if(total == M ){
+			return mid;
+		}
+		else if(buttom >= top-1){
+			if(getH(top) > M) return top;
+			else return mid;
+		}
+		else if(total > M){
+			return binarySearch(mid, top);
+		}
+		else{
+			return binarySearch(buttom, mid-1);
+		}
+	}
+	private static long getH(int h){
+		long sum = 0;
+		for(int i=0; i<WOODS.length; ++i){
+			if(WOODS[i] > h){
+				sum+=WOODS[i]-h;
+			}
+		}
+		return sum;
+	}
+}
